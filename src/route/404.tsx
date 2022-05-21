b5e5ce4227729a0c85162fda2a4a7207ff33e553
Pream Pinbut <momozahara@hotmail.com>
2022-05-22 04:09:11 +0700

    add 404 route

---
+++ src/route/404.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+export default () => {
+  return (
+    <Container className="text-center">
+      <h1>404</h1>
+      <p>Page not found</p>
+    </Container>
+  );
+};
--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import "./App.css";
 
 import Home from "./route/home";
 import Product from "./route/product";
+import NotFound from "./route/404";
 
 export default () => {
   let location = useLocation();
@@ -17,10 +18,12 @@ export default () => {
       >
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/404" element={<NotFound />} />
           <Route path="product" element={<Product />}>
             <Route path="house" />
             <Route path="apartment" />
           </Route>
+          <Route path="*" element={<Navigate replace to="/404" />} />
         </Routes>
       </CSSTransition>
       </TransitionGroup>
